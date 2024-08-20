1c58775faeb7e3ef3baa9c86efeaed74c4ca065f
bogdanmuh <muhutdinovbogdan97@gmail.com>
2024-08-20 18:09:07 +0300

    Добавил CompletableFutute

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public class Main {
 
-
-    public static void main(String[] args) {
-        int cores = Runtime.getRuntime().availableProcessors();
-        System.out.println("Количество доступных ядер: " + cores);
-
-        ShipGenerator shipGenerator = new ShipGenerator ();
-        shipGenerator.start();
-        PierLoader pierLoader = new PierLoader();
-        pierLoader.start();
+    public static void main(String[] args) throws InterruptedException {
+        System.out.printf("%s Генератор запустился%n", Thread.currentThread().getName());
+        List<CompletableFuture> futureList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            futureList.add(CompletableFuture
+                    .runAsync(() -> new Path().begin(generateShip()))
+                    .thenAccept(s -> System.out.println("end")));
+        }
+        futureList.forEach(CompletableFuture::join);
     }
+
+    public static Ship generateShip() {
+        return new Ship((int) (Math.random() * 100),
+                Product.values()[1],
+                10);
+    }
+///(int) (Math.random() * Product.values().length)
 }
--- src/main/java/org/example/PierLoader.java
+++ src/main/java/org/example/PierLoader.java
@@ -1,29 +1,20 @@
 package org.example;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PierLoader extends Thread {
-    private static HashMap<Product, Pier> piers = new HashMap<>();
-    private static Tunnel tunnel = new Tunnel();
+    private static final ConcurrentHashMap<Product, Pier> piers = new ConcurrentHashMap<>();
 
     static {
-        for(Product product : Product.values()) {
-            Pier pier = new Pier();
-            pier.start();
-            piers.put(product, pier);
+        for (Product product : Product.values()) {
+            piers.put(product, new Pier());
         }
     }
 
-    @Override
-    public void run() {
-        System.out.println("Балансировщику готов");
-        while (true) {
-            if (!tunnel.isEmpty()) {
-                Ship ship = tunnel.poll();
-                System.out.println(String.format("прибыл к балансировщику корабль с %s прибыл", ship.getProduct().name()));
-                piers.get(ship.getProduct()).add(ship);
-            }
-
-        }
+    public void add(Ship ship) {
+        System.out.printf("%s прибыл к балансировщику корабль с %s %n",
+                Thread.currentThread().getName(),
+                ship.getProduct().name());
+        piers.get(ship.getProduct()).unload(ship);
     }
 }
